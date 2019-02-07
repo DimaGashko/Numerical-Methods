cf25667b0829dd0ad03e8cea533feca02c270e4e
DimaGashko <dgashko@mail.ua>
2019-02-07 21:33:01 +0200

    matrixPransposeInit

---
+++ Lab1/src/functions/matrixTranspose.ts
@@ -0,0 +1,20 @@
+/**
+ * Возвращает транспонированную матрицу
+ * 
+ *```js
+ * matrixTranspose([
+ *    [1, 2, 3],
+ *    [4, 5, 6],
+ *    [7, 8, 9],
+ * ]); 
+ * // [1, 4, 7],
+ * // [2, 5, 8],
+ * // [3, 6, 9],
+ * ```
+ * 
+ * @param matrix матрица (двумерный массив)
+ * @returns транспонированная матрица
+ */
+export default function matrixTranspose(matrix: number[][]): number[][] {
+   return [];
+}
--- Lab1/src/index.ts
+++ Lab1/src/index.ts
@@ -1,7 +1,4 @@
 import 'normalize.css'
 import './styles/index.sass';
-import vecAbs from './functions/vecAbs';
 
 console.log('Numerical Methods: Lab 1');
-
-(<any>window).vecAbs = vecAbs; 
