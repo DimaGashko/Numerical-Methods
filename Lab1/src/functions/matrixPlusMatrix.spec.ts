ab8cfc92dda4ff49aac15f3d829c8e984e71ba25
DimaGashko <dgashko@mail.ua>
2019-02-07 23:05:00 +0200

    matrixPlusMatrix

---
+++ Lab1/src/functions/matrixPlusMatrix.spec.ts
@@ -0,0 +1,93 @@
+import { expect, assert } from 'chai';
+import matrixPlusMatrix from './matrixPlusMatrix';
+
+describe('matrixPlusMatrix', () => {
+   it('Ошибка при разном количестве строк', () => {
+      assert.throws(() => {
+         matrixPlusMatrix(
+            [
+               [1, 2],
+               [3, 4],
+            ], [
+               [1, 2],
+               [3, 4],
+               [5, 6],
+            ]
+         );
+
+      }, 'Matrixes must be the same length');
+   });
+   
+   it('Ошибка при разном количестве столбцов', () => {
+      assert.throws(() => {
+         matrixPlusMatrix(
+            [[1, 2, 3]], [[1]]
+         );
+
+      }, 'Matrixes must be the same length');
+   });
+
+   it('Матрицы 1х1', () => {
+      const res = matrixPlusMatrix([[5]], [[2]]);
+
+      expect(res).to.have.deep.members([[7]]);
+   });
+
+   it('Матрицы 2х2', () => {
+      const res = matrixPlusMatrix([
+         [1, 2],
+         [3, 4],
+      ], [
+         [2, 3],
+         [4, 5],
+      ]);
+
+      expect(res).to.have.deep.members([
+         [3, 5],
+         [7, 9],
+      ]);
+   });
+
+   it('Матрицы 2х3', () => {
+      const res = matrixPlusMatrix([
+         [1, 2, 3],
+         [4, 5, 6],
+      ], [
+         [6, 5, 4],
+         [3, 2, 1]
+      ]);
+
+      expect(res).to.have.deep.members([
+         [7, 7, 7],
+         [7, 7, 7],
+      ]);
+   });
+
+   it('Матрицы 3х1', () => {
+      const res = matrixPlusMatrix([
+         [1],
+         [2],
+         [3],
+      ], [
+         [2],
+         [2],
+         [2],
+      ]);
+
+      expect(res).to.have.deep.members([
+         [3],
+         [4],
+         [5],
+      ]);
+   });
+
+   it('Матрицы 1х4', () => {
+      const res = matrixPlusMatrix(
+         [[1, 2, 3, 4]],
+         [[2, 3, 4, 5]],
+      );
+
+      expect(res).to.have.deep.members([[3, 5, 7, 9]]);
+   });
+
+});
--- Lab1/src/functions/matrixPlusMatrix.ts
+++ Lab1/src/functions/matrixPlusMatrix.ts
@@ -3,23 +3,32 @@
  * 
  *```js
  * matrixMulNum([
-   *    [1, 2, 3],
-   *    [4, 5, 6],
-   *    [7, 8, 9],
-   * ], [
-   *  [9, 8, 7],
-   *  [6, 5, 4],
-   *  [3, 2, 1],
-   * ]); 
-   * // [10, 10, 10],
-   * // [10, 10, 10],
-   * // [10, 10, 10],
-   * ```
-   * 
-   * @param a первая матрица (в виде двумерного массива)
-   * @param b вторая матрица (в виде двумерного массива)
-   * @returns сумма двух матриц
-  */
-  export default function matrixPlusMatrix(matrix: number[][], num: number): number[][] {
-     return [];
-  }
+ *    [1, 2, 3],
+ *    [4, 5, 6],
+ *    [7, 8, 9],
+ * ], [
+ *  [9, 8, 7],
+ *  [6, 5, 4],
+ *  [3, 2, 1],
+ * ]); 
+ * // [10, 10, 10],
+ * // [10, 10, 10],
+ * // [10, 10, 10],
+ * ```
+ * 
+ * @param a первая матрица (в виде двумерного массива)
+ * @param b вторая матрица (в виде двумерного массива)
+ * @returns сумма двух матриц
+ */
+export default function matrixPlusMatrix(a: number[][], b: number[][]): number[][] {
+   const m1 = a.length;
+   const m2 = b.length;
+
+   if (m1 && m2 && (m1 !== m2 || a[0].length !== b[0].length)) {
+      throw 'Matrixes must be the same length';
+   } 
+   
+   return a.map((row, i) => {
+      return row.map((item, j) => item + b[i][j]);
+   });
+}
