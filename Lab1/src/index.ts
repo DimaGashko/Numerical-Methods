002cf17d5eb3ce488f9f99f3d354b086c7377fc3
DimaGashko <dgashko@mail.ua>
2019-02-09 19:37:35 +0200

    MatrixDomDone

--- Lab1/src/index.ts
+++ Lab1/src/index.ts
@@ -3,9 +3,7 @@ import MatrixDom from './components/MatrixDom/MatrixDom';
 import 'normalize.css'
 import './styles/index.sass';
 
-const matrixDom = new MatrixDom({
-   disabled: true,
-});
+const matrixDom = new MatrixDom();
 
 //matrixDom.minM = 1;
 //matrixDom.maxM = 1; 
