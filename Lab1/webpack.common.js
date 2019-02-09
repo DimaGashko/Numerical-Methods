3db5eaaa25488fb932c5245bda489a5199dd7104
DimaGashko <dgashko@mail.ua>
2019-02-09 23:49:40 +0200

    distStaticServer

--- Lab1/webpack.common.js
+++ Lab1/webpack.common.js
@@ -25,7 +25,7 @@ module.exports = {
    output: {
       filename: '[name].[contenthash].js',
       path: path.resolve(__dirname, 'dist'),
-      //publicPath: '/',
+      publicPath: '/',
    },
    module: {
       rules: [{
